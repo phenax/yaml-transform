5264e2ea03ec8d9d862de38f5ffbe3780e57fca1
Akshay Nair <phenax5@gmail.com>
2024-04-14 17:50:56 +0530

    Add sequences

--- specs/Specs/ParserSpec.hs
+++ specs/Specs/ParserSpec.hs
@@ -1,5 +1,7 @@
 module Specs.ParserSpec where
 
+import qualified Data.Text as Text
+import qualified Data.Text.Lazy as LazyText
 import qualified Debug.Trace as Debug
 import NeatInterpolation (text)
 import Test.Hspec
@@ -8,6 +10,35 @@ import YamlTransform.Parser (Yaml (..), parse)
 test :: SpecWith ()
 test = do
   describe "YamlTransform.Parser" $ do
+    describe "valid keys" $ do
+      it "allows valid keys" $ do
+        parse "-he-llo-: world"
+          `shouldBe` Right
+            [ YMLMapping
+                "-he-llo-"
+                [ YMLWSSpace,
+                  YMLScalar "world"
+                ]
+            ]
+        parse "_h_ello_: world"
+          `shouldBe` Right
+            [ YMLMapping
+                "_h_ello_"
+                [ YMLWSSpace,
+                  YMLScalar "world"
+                ]
+            ]
+        parse "$he$llo$: world"
+          `shouldBe` Right
+            [ YMLMapping
+                "$he$llo$"
+                [ YMLWSSpace,
+                  YMLScalar "world"
+                ]
+            ]
+        -- TODO: Better error messages
+        parse "he#llo: world" `shouldBe` Left "Failed reading: empty"
+
     describe "with mappings" $ do
       context "when input contains a single mapping raw text mapping" $ do
         it "creates mapping" $ do
@@ -20,31 +51,28 @@ test = do
                   ]
               ]
 
-      describe "valid keys" $ do
-        it "creates mapping" $ do
-          parse "-he-llo-: world"
+      context "when input contains a multiple mappings" $ do
+        it "succeeds with mapping" $ do
+          let input =
+                [text|
+                hello: world
+                foo:
+                  bar: is foo
+                another-item: 123
+                |]
+          parse input
             `shouldBe` Right
-              [ YMLMapping
-                  "-he-llo-"
-                  [ YMLWSSpace,
-                    YMLScalar "world"
-                  ]
-              ]
-          parse "_h_ello_: world"
-            `shouldBe` Right
-              [ YMLMapping
-                  "_h_ello_"
-                  [ YMLWSSpace,
-                    YMLScalar "world"
-                  ]
-              ]
-          parse "$he$llo$: world"
-            `shouldBe` Right
-              [ YMLMapping
-                  "$he$llo$"
-                  [ YMLWSSpace,
-                    YMLScalar "world"
-                  ]
+              [ YMLMapping "hello" [YMLWSSpace, YMLScalar "world"],
+                YMLNewLine,
+                YMLMapping
+                  "foo"
+                  [ YMLNewLine,
+                    YMLWSSpace,
+                    YMLWSSpace,
+                    YMLMapping "bar" [YMLWSSpace, YMLScalar "is foo"],
+                    YMLNewLine
+                  ],
+                YMLMapping "another-item" [YMLWSSpace, YMLScalar "123"]
               ]
 
       context "when input contains a complex nested mappings" $ do
@@ -128,32 +156,8 @@ test = do
                   ]
               ]
 
-      context "when input contains a multiple mappings" $ do
-        it "succeeds with mapping" $ do
-          let input =
-                [text|
-                hello: world
-                foo:
-                  bar: is foo
-                another-item: 123
-                |]
-          parse input
-            `shouldBe` Right
-              [ YMLMapping "hello" [YMLWSSpace, YMLScalar "world"],
-                YMLNewLine,
-                YMLMapping
-                  "foo"
-                  [ YMLNewLine,
-                    YMLWSSpace,
-                    YMLWSSpace,
-                    YMLMapping "bar" [YMLWSSpace, YMLScalar "is foo"],
-                    YMLNewLine
-                  ],
-                YMLMapping "another-item" [YMLWSSpace, YMLScalar "123"]
-              ]
-
     describe "with comments" $ do
-      context "when a value includes a comment at the end" $ do
+      context "when a value includes a comment at the end of a scalar value" $ do
         it "recognizes it as a comment" $ do
           parse "hello: This is some text # and then some more text"
             `shouldBe` Right
@@ -163,6 +167,26 @@ test = do
                     YMLScalar "This is some text",
                     YMLWSSpace,
                     YMLComment " and then some more text"
+                  ]
+              ]
+
+      context "when a value includes a comment at the end of mapping key" $ do
+        it "recognizes it as a comment" $ do
+          let input =
+                [text|
+                hello: # This is a comment
+                  world: 123
+                |]
+          parse input
+            `shouldBe` Right
+              [ YMLMapping
+                  "hello"
+                  [ YMLWSSpace,
+                    YMLComment " This is a comment",
+                    YMLNewLine,
+                    YMLWSSpace,
+                    YMLWSSpace,
+                    YMLMapping "world" [YMLWSSpace, YMLScalar "123"]
                   ]
               ]
 
@@ -199,4 +223,78 @@ test = do
                     YMLWSSpace,
                     YMLComment " and # then #some more# text#"
                   ]
+              ]
+
+    describe "with sequences" $ do
+      context "when there is a sequence at root" $ do
+        it "creates a sequence of scalars at root" $ do
+          let input =
+                [text|
+            - hello
+            - world
+            - 123
+            |]
+          parse input
+            `shouldBe` Right
+              [ YMLSequenceItem [YMLWSSpace, YMLScalar "hello"],
+                YMLNewLine,
+                YMLSequenceItem [YMLWSSpace, YMLScalar "world"],
+                YMLNewLine,
+                YMLSequenceItem [YMLWSSpace, YMLScalar "123"]
+              ]
+
+      context "when sequence contains a mapping" $ do
+        it "creates a nested mapping within the sequence" $ do
+          let input =
+                [text|
+            - hello: world
+              foo: bar
+            - 123
+            |]
+          parse input
+            `shouldBe` Right
+              [ YMLSequenceItem
+                  [ YMLWSSpace,
+                    YMLMapping "hello" [YMLWSSpace, YMLScalar "world"],
+                    YMLNewLine,
+                    YMLWSSpace,
+                    YMLWSSpace,
+                    YMLMapping "foo" [YMLWSSpace, YMLScalar "bar"],
+                    YMLNewLine
+                  ],
+                YMLSequenceItem [YMLWSSpace, YMLScalar "123"]
+              ]
+
+      context "when mapping contains a sequence" $ do
+        it "creates nested sequence + mapping" $ do
+          let input =
+                [text|
+                hello:
+                  - salad
+                  - this: that
+                  - fries
+                foo: bar
+                |]
+          parse input
+            `shouldBe` Right
+              [ YMLMapping
+                  "hello"
+                  [ YMLNewLine,
+                    YMLWSSpace,
+                    YMLWSSpace,
+                    YMLSequenceItem [YMLWSSpace, YMLScalar "salad"],
+                    YMLNewLine,
+                    YMLWSSpace,
+                    YMLWSSpace,
+                    YMLSequenceItem
+                      [ YMLWSSpace,
+                        YMLMapping "this" [YMLWSSpace, YMLScalar "that"],
+                        YMLNewLine
+                      ],
+                    YMLWSSpace,
+                    YMLWSSpace,
+                    YMLSequenceItem [YMLWSSpace, YMLScalar "fries"],
+                    YMLNewLine
+                  ],
+                YMLMapping "foo" [YMLWSSpace, YMLScalar "bar"]
               ]
--- src/YamlTransform/Parser.hs
+++ src/YamlTransform/Parser.hs
@@ -1,7 +1,6 @@
 module YamlTransform.Parser (parse, Yaml (..)) where
 
 import Control.Applicative (optional)
-import Control.Arrow ((&&&))
 import Control.Monad (join, unless, void, when)
 import qualified Data.Attoparsec.Combinator as P
 import qualified Data.Attoparsec.Text as P
@@ -12,7 +11,7 @@ import qualified Debug.Trace as Debug
 
 data Yaml
   = YMLMapping Text [Yaml]
-  | YMLSequence [Yaml]
+  | YMLSequenceItem [Yaml]
   | YMLScalar Text -- TODO: Add scalar type
   | YMLAlias Text
   | YMLComment Text
@@ -25,9 +24,6 @@ debugNextChar :: P.Parser ()
 debugNextChar =
   P.lookAhead P.anyChar >>= (Debug.traceM . ("[Next char] " ++) . show)
 
-diverge :: (a -> b) -> (a -> c) -> a -> (b, c)
-diverge = (&&&)
-
 takeUntilParsable :: P.Parser a -> P.Parser Text
 takeUntilParsable p = do
   t <- P.lookAhead $ optional p
@@ -39,29 +35,30 @@ takeUntilParsable p = do
 
 inlineScalarP :: P.Parser Yaml
 inlineScalarP =
-  YMLScalar <$> takeUntilParsable endOfInlineP
+  YMLScalar <$> takeUntilParsable endOfInlineValue
   where
-    endOfInlineP =
+    endOfInlineValue =
       P.choice
         [ P.endOfLine,
           P.endOfInput,
-          void $ whitespaceCharP >> P.char '#' >> whitespaceCharP
+          void $ whitespaceP >> P.char '#' >> whitespaceP
         ]
 
 identifierP :: P.Parser Text
 identifierP = P.takeWhile1 (P.inClass "a-zA-Z0-9-_$")
 
-mappingP :: Int -> P.Parser [Yaml]
-mappingP currentIndent = do
-  (indent, whitespaces) <- diverge length id <$> P.many' whitespaceP
-  when (indent <= currentIndent) $ fail "Invalid indent level"
+mappingP :: Int -> Int -> P.Parser [Yaml]
+mappingP parentIndent prefixIndent = do
+  whitespaces <- P.many' whitespaceP
+  let indent = length whitespaces + prefixIndent
+  when (indent <= parentIndent) $ fail "Invalid indent level"
   key <- identifierP <* P.char ':'
   (whitespaces ++) . pure . YMLMapping key <$> valueP indent
   where
     valueP indent = P.choice [objectValueP indent, inlineValueP]
     objectValueP indent = do
       comment <- endOfLineIgnorableP
-      value <- yamlP indent
+      value <- yamlP indent 0
       pure $ comment ++ value
     inlineValueP = do
       spaces <- P.many1 whitespaceP
@@ -69,18 +66,29 @@ mappingP currentIndent = do
       comment <- fromMaybe [] <$> optional (P.lookAhead whitespaceP >> endOfLineIgnorableP)
       pure $ spaces ++ [value] ++ comment
 
-whitespaceCharP :: P.Parser Char
-whitespaceCharP = P.satisfy $ P.inClass " \t"
+sequenceP :: Int -> P.Parser [Yaml]
+sequenceP parentIndent = do
+  whitespaces <- P.many' whitespaceP
+  let indent = length whitespaces
+  when (indent <= parentIndent) $ fail "Invalid indent level"
+  P.char '-'
+  values <- P.choice [yamlP indent indent, inlineValueP]
+  pure $ whitespaces ++ [YMLSequenceItem values]
+  where
+    inlineValueP = do
+      spaces <- P.many1 whitespaceP
+      value <- inlineScalarP
+      comment <-
+        fromMaybe []
+          <$> optional (P.lookAhead whitespaceP >> endOfLineIgnorableP)
+      pure $ spaces ++ [value] ++ comment
 
 newlineP :: P.Parser Yaml
 newlineP = YMLNewLine <$ P.endOfLine
 
 whitespaceP :: P.Parser Yaml
 whitespaceP =
-  P.choice
-    [ YMLWSSpace <$ P.char ' ',
-      YMLWSTab <$ P.char '\t'
-    ]
+  P.choice [YMLWSSpace <$ P.char ' ', YMLWSTab <$ P.char '\t']
 
 endOfLineIgnorableP :: P.Parser [Yaml]
 endOfLineIgnorableP = do
@@ -91,14 +99,19 @@ endOfLineIgnorableP = do
 
 commentP :: P.Parser Yaml
 commentP = do
-  P.char '#' >> P.lookAhead whitespaceCharP
+  P.char '#' >> P.lookAhead whitespaceP
   YMLComment <$> P.takeWhile1 (not . P.isEndOfLine)
 
-yamlP :: Int -> P.Parser [Yaml]
-yamlP level = join <$> P.many1 yamlPart
+yamlP :: Int -> Int -> P.Parser [Yaml]
+yamlP parentIndent prefixIndent = join <$> P.many1 yamlPart
   where
-    -- inlineScalarP
-    yamlPart = P.choice [P.many1 newlineP, P.many1 commentP, mappingP level]
+    yamlPart =
+      P.choice
+        [ P.many1 newlineP,
+          P.many1 commentP,
+          sequenceP parentIndent,
+          mappingP parentIndent prefixIndent
+        ]
 
 leftOverP :: P.Parser ()
 leftOverP = do
@@ -107,4 +120,4 @@ leftOverP = do
   P.endOfInput
 
 parse :: Text -> Either String [Yaml]
-parse = P.parseOnly (yamlP (-1) <* leftOverP)
+parse = P.parseOnly (yamlP (-1) 0 <* leftOverP)
